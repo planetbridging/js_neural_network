f6733a0bda30d06a17b224640d467ada199f4449
planetbridging <47130739+planetbridging@users.noreply.github.com>
2023-07-07 13:57:46 +1000

    pushed to linux & different training data

--- index.js
+++ index.js
@@ -12,15 +12,59 @@ var objFileManager = require("./fileManager");
 async function trainNeuralNetwork() {
   var oFileManager = new objFileManager();
 
+  await oFileManager.setupTrainData("train.csv");
+  var setupData = oFileManager.setupInputsTargets();
+
+  const inputs = setupData[0];
+  const targets = setupData[1];
+
+  console.log("one sample from inputs");
+  console.log(inputs[0]);
+
+  console.log("one sample from targets");
+  console.log(targets[0]);
+
+  // Create a new neural network instance
+  const net = new brain.NeuralNetwork({
+    hiddenLayers: [20, 20],
+  });
+
+  // Prepare the training data
+  const trainingData = inputs.map((input, index) => {
+    return {
+      input: input,
+      output: targets[index],
+    };
+  });
+
+  // Train the network
+  await net.trainAsync(trainingData, {
+    errorThresh: 0.025,
+    log: true,
+    logPeriod: 1,
+    learningRate: 0.1,
+  });
+
+  var json = net.toJSON();
+  oFileManager.saveJsonFile("weights.json", json);
+
+  // You can now use the trained network
+  const output = net.run(inputs[0]); // for example
+  console.log(output);
+}
+
+async function trainNeuralNetwork1() {
+  var oFileManager = new objFileManager();
+
   var data = await oFileManager.readFile("train.csv");
   var trainData = oFileManager.getMnistData(data);
 
-  var net = new brain.NeuralNetwork({ hiddenLayers: [784, 392, 196] });
+  console.log(trainData);
 
-  net.trainAsync(trainData, {
-    errorThresh: 0.045,
+  var net = new brain.NeuralNetwork({ hiddenLayers: [5, 5, 5] });
+
+  await net.trainAsync(trainData, {
     log: true,
-    logPeriod: 1,
     learningRate: 0.1,
     iterations: 2000,
   });
